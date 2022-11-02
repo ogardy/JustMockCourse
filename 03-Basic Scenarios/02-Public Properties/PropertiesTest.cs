f880f868113d39beabd49ddcc6667fc36ef2d6d1
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2022-11-02 10:24:45 -0400

    Add Basic Scenarios - Public Properties

---
+++ 03-Basic Scenarios/02-Public Properties/PropertiesTest.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+
+namespace JustMockCourse.BasicScenarios.PublicProperties;
+
+public interface IEntity
+{
+  int Id { get; set; }
+}
+
+public class PropertiesTest
+{
+  [Fact]
+  public void ShouldFakePropertyGet()
+  {
+    // Arrange 
+    IEntity entity = null;
+
+    // Act 
+    int actual = entity.Id;
+
+    // Assert 
+    actual.Should().Be(25);
+  }
+
+  [Fact]
+  public void ShouldAssertPropertySet()
+  {
+    // Arrange 
+    IEntity entity = null;
+
+    // Act 
+    entity.Id = 1;
+
+    // Assert 
+    // Mock.AssertSet(() => entity.Id = 1);
+  }
+
+  [Fact]
+  public void ShouldThrowExceptionOnTheThirdPropertySetCall()
+  {
+    // Arrange 
+    IEntity entity = null;
+
+    // Act 
+    entity.Id = 4;
+    entity.Id = 5;
+
+    void act() => entity.Id = 15;
+
+    // Assert 
+    // act.Should().Throw<StrictMockException>();
+  }
+}
